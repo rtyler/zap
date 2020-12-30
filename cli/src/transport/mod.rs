0bdd2ba9bcfeaa0fb985a8a723d1fc0adf860f28
R. Tyler Croy <rtyler@brokenco.de>
2020-12-29 22:16:52 -0800

    Add some sketches of what tasks and plans might look like

--- cli/src/transport/mod.rs
+++ cli/src/transport/mod.rs
@@ -2,6 +2,10 @@ use crate::inventory::{Group, Inventory, Target};
 
 pub mod ssh;
 
+/**
+ * The Transport trait allows for multiple transports to be implemented for
+ * connecting to targets
+ */
 pub trait Transport {
     fn run_group(&self, cmd: &str, group: &Group, inv: &Inventory) -> i32;
     fn run(&self, command: &str, target: &Target) -> i32;
