b00e9835e8d6c194860c4584d4070f1a7dfdc559
R. Tyler Croy <rtyler@brokenco.de>
2020-12-30 14:40:52 -0800

    Allow parameters inside of a task to be optional.
    I can imagine a few cases where there wouldn't be any parameters needed

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -26,8 +26,18 @@ mod tests {
                 script {
                     inline = "zypper in -y ${ZAP_PACKAGE}"
                 }
-            }
-        "#;
+            }"#;
+        let _task = TaskParser::parse(Rule::task, buf)
+            .unwrap().next().unwrap();
+    }
+
+    #[test]
+    fn parse_no_parameters() {
+        let buf = r#"task PrintEnv {
+                script {
+                    inline = "env"
+                }
+            }"#;
         let _task = TaskParser::parse(Rule::task, buf)
             .unwrap().next().unwrap();
     }
